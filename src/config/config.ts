23c1483ee5a88360d996059e43b1328326f21ae7
Mauri <mauricio.cucco@estoes.me>
2022-05-15 23:54:33 -0300

    Fix del archivo config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,29 +1,26 @@
 import dotenv from 'dotenv';
 
-// Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFound = dotenv.config();
 
 if (envFound.error) {
-    // This error should crash whole process
-    throw new Error("⚠️  Couldn't find .env file  ⚠️");
+    throw new Error('⚠️ No se pudo encontrar el archivo .env ⚠️');
 }
 
 export default {
+    //Server
     port: process.env.PORT as string,
-    /**
-     * DATABASE
-     */
+
+    //Conexión a la base de datos
     dbName: process.env.DB_NAME as string,
     dbUser: process.env.DB_USER as string,
     dbHost: process.env.DB_HOST as string,
     dbDriver: process.env.DB_DRIVER,
     dbPassword: process.env.DB_PASSWORD as string,
     dbPort: parseInt(process.env.DB_PORT as string, 10),
-    /**
-     * API configs
-     */
+
+    //API
     api: {
         prefix: '/api',
     },
